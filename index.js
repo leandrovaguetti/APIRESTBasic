35b56dfc37ae7b893eff84b4df12505e2bedfaf5
LeandroVaguetti <12098260-LeandroVaguetti@users.noreply.replit.com>
2023-05-18 11:24:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.send('Alô REST API');
+});
+
+
+app.get('/api/echo/:param*', (req, res) => {
+	res.send(req.params.param);
+});
+
+app.get('/api/echodbl/:param1/:param2', (req, res) => {
+	res.send(req.params.param1 + ', ' + req.params.param2);
+});
+
+app.get('/api/echoquery/*', (req, res) => {
+	res.send(req.query.q);
+});
+
+app.get('/api', (req, res) => {
+	res.send('API ativa !!!');  
+});
+
+const users = [
+  {name: 'Jones', email: 'jones@gmail.com'},
+  {name: 'Henrique', email: 'henrique@hotmail.com'}
+]
+
+app.get('/users', (req, res) => {
+	res.json(users);
+});
+
+app.get('/api/echobody', (req,res) => {
+  res.send(req.body);
+});
+
+app.listen(3000,()=> console.log('server starter'));
+
